4fe34d1ed804e02024775f6f0d5fc49ee40b367f
vro <89111436+skidvro@users.noreply.github.com>
2024-03-13 23:58:37 -0400

    Update userinfo.py

--- userinfo.py
+++ userinfo.py
@@ -3,7 +3,10 @@ async def userinfo(ctx, user_id: int):
     try:
         user = await bot.fetch_user(user_id)
         created_at = user.created_at.strftime("%Y-%m-%d %H:%M:%S")
-        joined_at = ctx.guild.get_member(user.id).joined_at.strftime("%Y-%m-%d %H:%M:%S")
+        if ctx.guild:
+            joined_at = ctx.guild.get_member(user.id).joined_at.strftime("%Y-%m-%d %H:%M:%S")
+        else:
+            joined_at = "Not applicable in DMs"
         badges = ", ".join(badge.name.replace('_', ' ').title() for badge in user.public_flags.all())
         
         info = f"ID: {user.id}\n"
@@ -14,6 +17,8 @@ async def userinfo(ctx, user_id: int):
         info += f"Server Join Date: {joined_at}\n"
         info += f"Badges: {badges or 'None'}"
 
-        await ctx.send(f"```{info}```")
+        message = await ctx.send(f"```{info}```")
+        await asyncio.sleep(1)
+        await message.delete()
     except discord.NotFound:
         await ctx.send("User not found.")
